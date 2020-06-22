60f57fc80819abbd21c54b86815ec1f7ebb3125e
Gideon-coder <gomotosho1@gmail.com>
2020-06-22 13:35:35 +0100

    chore(server.js):update server.js script

--- server/server.js
+++ server/server.js
@@ -1,31 +1,36 @@
-const express = require('express');
-const favicon = require('express-favicon');
+const express = require("express");
+const favicon = require("express-favicon");
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '../build/favicon.ico'));
+// app.use(favicon(__dirname + '../build/favicon.ico'));
+const path = require("path");
 
+app.use(favicon(path.join(__dirname, "../", "build", "favicon.ico")));
 
+const isProduction = process.env.NODE_ENV === undefined || "undefined";
 
-const isProduction = process.env.NODE_ENV === 'production';
-
+console.log(process.env.NODE_ENV);
 
 if (isProduction) {
-    app.use(express.static('build'));
-    // eslint-disable-next-line global-require
-    const path = require('path');
-  
-    app.get("/*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../",'build', 'index.html'));
-    });
-  }
+  app.use(express.static("build"));
+  // eslint-disable-next-line global-require
+  const path = require("path");
 
-
-  /* Redirect http to https */
-app.get('*', (req, res, next) => {
-    if (req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production') res.redirect(`https://${req.hostname}${req.url}`);
-    else next(); /* Continue to other routes if we're not redirecting */
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../", "build", "index.html"));
   });
+}
 
-app.listen(port,()=>{
-    console.log("Application started at port:%d",port);
+/* Redirect http to https */
+app.get("*", (req, res, next) => {
+  if (
+    req.headers["x-forwarded-proto"] != "https" &&
+    process.env.NODE_ENV === "production"
+  )
+    res.redirect(`https://${req.hostname}${req.url}`);
+  else next(); /* Continue to other routes if we're not redirecting */
+});
+
+app.listen(port, () => {
+  console.log("Application started at port:%d", port);
 });
--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Login from "../src/container/program/login";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Register from "../src/container/program/Register";
+import UserProfile from "../src/container/userSection/userProfileSetup";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/login" component={Login}></Route>
+        <Route path="/" component={Login}></Route>
         <Route path="/register" component={Register}></Route>
+        <Route path="/userprofile" component={UserProfile}></Route>
       </Switch>
     </BrowserRouter>
   );
