c8b898c978753bf64928aeb41b9eb21cfbb81831
Gideon-coder <gomotosho1@gmail.com>
2020-06-22 11:06:47 +0100

    feature(Desktop Register Page)add add Desktop Register page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Login from "../src/container/program/login";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Register from "../src/container/program/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Login}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/register" component={Register}></Route>
       </Switch>
     </BrowserRouter>
   );
